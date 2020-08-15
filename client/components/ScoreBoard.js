ca058b4fe7106d07df8f8d1c985eba23ae1b5957
Alonso Garza <alonsogarza@Alonsos-MacBook-Pro.local>
2020-08-15 13:30:06 -0500

    Worked on sign in page.

--- client/components/ScoreBoard.js
+++ client/components/ScoreBoard.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FriendContainer from './FriendContainer';
 
 const ScoreBoard = (props) => {
-  return <div id='score-board'>ScoreBoard!!</div>;
-  <FriendContainer />;
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('Rob');
+  return (
+    <div id='score-board'>
+      {count}
+      {name}
+      <button
+        onClick={() => {
+          setName(name + 's'), setCount(count + 1);
+        }}
+      >
+        Click me
+      </button>
+      <br></br>
+      ScoreBoard!! <FriendContainer />;
+    </div>
+  );
 };
 
 export default ScoreBoard;
--- client/containers/LoginContainer.js
+++ client/containers/LoginContainer.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginContainer = (props) => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div id='login-container'>
-      Name:<input></input>
-      Passowrd:<input></input>
+      <form
+        action=''
+        value='Update'
+        onSubmit={(e) => {
+          e.preventDefault();
+          props.setUsername(name);
+          console.log(name);
+        }}
+      >
+        <div>
+          <label>User:</label>
+          <input type='text' onChange={(e) => setName(name + e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type='password'
+            onChange={(e) => setPassword(password + e.target.value)}
+          />
+        </div>
+        <br></br>
+        <center>
+          <input className='myButton' type='submit' />
+        </center>
+      </form>
     </div>
   );
 };
