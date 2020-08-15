cace76f555cea960b335f9c6677f0549f0a44f51
Rob Nobile <robert.nobile@gmail.com>
2020-08-15 18:49:46 -0400

    Setup useEffect fetch

--- client/App.js
+++ client/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginContainer from './containers/LoginContainer';
 import NavContainer from './containers/NavContainer';
 import MainContainer from './containers/MainContainer';
 
 const App = () => {
   const [user, setUsername] = useState('');
+  const [userInfo, setUserInfo] = useState([]);
   const codeWarsData = {
     username: 'Alonsog66',
     name: null,
@@ -34,10 +35,26 @@ const App = () => {
     },
   };
 
+  useEffect(() => {
+      fetch(
+        'https://www.codewars.com/api/v1/users/sull364', {
+          headers: {
+            Authorization: 'qfkQ9s6yV1sAic6RhoKN'
+          }
+        }
+      )
+      .then(response => response.json())
+      // const data =  result.json();
+      .then(data => {
+        console.log('HEY I AM THE FETCH RESULTS: ', data);
+        setUserInfo(data)
+      })
+      .catch(err => console.log('Logged err:', err));
+  }, []);
+
   return (
     <div id='app'>
       <LoginContainer setUsername={setUsername} />
-
       <NavContainer username={codeWarsData.username} />
       <MainContainer codeWarsData={JSON.stringify(codeWarsData)} />
     </div>
